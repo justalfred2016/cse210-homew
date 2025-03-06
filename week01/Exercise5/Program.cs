20099c0d7dfccf2ac52a58d11f83277f50280d69
justalfred2016 <stlokotsi@byupathway.edu>
2025-03-06 06:18:21 +0200

    CShapier

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,6 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
+
+    //call all the functions
      DisplayMessage();
 
      String UserName =PromptUserName();
@@ -17,28 +19,31 @@ class Program
 
     
     }
-    
+    //Create a function that displays a message
     static void DisplayMessage()
     {
         Console.WriteLine("Welcome to the program!");
     }
+    //Create a function that stores and returns a name
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
     }
+    //Create a function that stores and returns a number
     static int PromptUserNumber()
     {
         Console.Write("Please enter Your favorite number: ");
         int numbers = int.Parse(Console.ReadLine());
         return numbers;
     } 
-
+    //Create a function that creates a reference a number from the a function and spuares and stores it.
     static int SquareNumber(int num){
        int square = num * num;
        return square;
     }
+    //Create a function that reference the squared number and the name from aboove and displays them.
     static void DisplayResults(string name, int square){
         Console.WriteLine($"{name}, the square of your number is {square} ");
     }
