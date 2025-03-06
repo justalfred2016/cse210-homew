f06d97df53d64275c2cf07574e76ed5fae8bc116
justalfred2016 <stlokotsi@byupathway.edu>
2025-03-06 06:32:23 +0200

    fixes

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,25 +5,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        //call all the functions
+        DisplayMessage();
 
-    //call all the functions
-     DisplayMessage();
+        String UserName = PromptUserName();
 
-     String UserName =PromptUserName();
+        int num = PromptUserNumber();
 
-     int num =PromptUserNumber();
+        int square = SquareNumber(num);
 
-     int square = SquareNumber(num);
-
-     DisplayResults(UserName, square);
-
-    
+        DisplayResults(UserName, square);
     }
+
     //Create a function that displays a message
     static void DisplayMessage()
     {
         Console.WriteLine("Welcome to the program!");
     }
+
     //Create a function that stores and returns a name
     static string PromptUserName()
     {
@@ -31,21 +30,25 @@ class Program
         string name = Console.ReadLine();
         return name;
     }
+
     //Create a function that stores and returns a number
     static int PromptUserNumber()
     {
         Console.Write("Please enter Your favorite number: ");
         int numbers = int.Parse(Console.ReadLine());
         return numbers;
-    } 
-    //Create a function that creates a reference a number from the a function and spuares and stores it.
-    static int SquareNumber(int num){
-       int square = num * num;
-       return square;
     }
+
+    //Create a function that creates a reference a number from the a function and spuares and stores it.
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+
     //Create a function that reference the squared number and the name from aboove and displays them.
-    static void DisplayResults(string name, int square){
+    static void DisplayResults(string name, int square)
+    {
         Console.WriteLine($"{name}, the square of your number is {square} ");
     }
-    
 }
